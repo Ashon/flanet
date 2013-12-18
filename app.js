c9af75569a7043e6535f6b5a053ce0add23763f2
ashon <ashon8813@gmail.com>
2013-12-18 13:24:10 +0900

    everything changed..

--- app.js
+++ app.js
@@ -1,51 +1,128 @@
-var async = require('async');
-var express = require('express');
-var util = require('util');
-var faceplate = require('faceplate');
-var path = require('path');
-// create an express webserver
-var app = express();
+var express = require('express')
+  , util = require('util')
+  , passport = require('passport')
+  , fbStrategy = require('passport-facebook').Strategy;
 
 var FACEBOOK_APP_ID = '1400435640201754';
 var FACEBOOK_SECRET = 'c5bfd96f8dec9520f1dd1d5795c2d623';
 
-var port = process.env.PORT || 3000;
-
-app.configure(function(){
-  app.set('id', FACEBOOK_APP_ID);
-  app.set('name', 'flanet');
-  app.set('port', port);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.set('view options', { pretty: true });
-  app.locals.pretty = true;
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('secret'));
-  app.use(express.session({ secret: 'secret' }));
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use(faceplate.middleware({
-    app_id: FACEBOOK_APP_ID,
-    secret: FACEBOOK_SECRET,
-    scope: 'user_likes, user_photos, user_status, user_activities, publish_actions, user,goups, user_subscriptions'
-  }));
-
-  app.use(function(req, res, next){
-    res.locals.host = req.headers['host'];
-    res.locals.scheme = req.headers['x-forwarded-proto'] || 'http';
-    res.locals.url = function(path){
-      return res.locals.scheme + res.locals.url_no_scheme(path);
-    };
-    res.locals.url_no_scheme = function(path){
-      return '://' + res.locals.host + path;
-    };
-    next();
-  });
-
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Facebook profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
 });
 
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the FacebookStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Facebook
+//   profile), and invoke a callback with a user object.
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's Facebook profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Facebook account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.logger());
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  // Initialize Passport!  Also use passport.session() middleware, to support
+  // persistent login sessions (recommended).
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user });
+});
+
+
+// GET /auth/facebook
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Facebook authentication will involve
+//   redirecting the user to facebook.com.  After authorization, Facebook will
+//   redirect the user back to this application at /auth/facebook/callback
+app.get('/auth/facebook',
+  passport.authenticate('facebook'),
+  function(req, res){
+    // The request will be redirected to Facebook for authentication, so this
+    // function will not be called.
+  });
+
+// GET /auth/facebook/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+
+app.listen(port, function(){
+  console.log("http listen : " + port);
+});
+
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
+
+/*
 function render_page(req, res) {
   req.facebook.app(function(app) {
     console.log(app);
@@ -54,7 +131,7 @@ function render_page(req, res) {
         "layout":    false,
         "req":       req,
         "app":       {"id":FACEBOOK_APP_ID,"name":"flanet"},
-        "user":      user
+        "user":      req.user
       });
     });
   });
@@ -108,8 +185,4 @@ function handle_facebook_request(req, res) {
 }
 
 app.get('/', handle_facebook_request);
-app.post('/', handle_facebook_request);
-
-app.listen(port, function() {
-  console.log("Listening on " + port + "\napp : " + app);
-});
+app.post('/', handle_facebook_request);*/
