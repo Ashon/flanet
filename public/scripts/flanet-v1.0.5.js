a65e72f93b2579db6d229383a3b792981a3c2614
ashon <ashon8813@gmail.com>
2013-12-19 17:55:27 +0900

    .

--- public/scripts/flanet-v1.0.5.js
+++ public/scripts/flanet-v1.0.5.js
@@ -258,7 +258,9 @@ Flanet.Panel.prototype = {
 			height : that.height * 2 - sWidth,
 			fill : "#fff",
 			image : img,
-			stroke : "#f60",
+			stroke : '#' + (function co(lor){   return (lor +=
+  [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])
+  && (lor.length == 6) ?  lor : co(lor); })(''),
 			strokeWidth : sWidth,
 			centerOffset : {
 				x : that.width - sWidth / 2,
