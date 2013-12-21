fd34bcc3822a13e7d25f4ba4c3c43edb1530ce06
ashon <ashon8813@gmail.com>
2013-12-21 23:36:41 +0900

    .

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@
 */
 
 exports.index = function(req, res){
-	res.render('index', { app : {id : FACEBOOK_APP_ID}, user: req.user });
+	res.render('index', { app : {id : process.env.FACEBOOK_APP_ID}, user: req.user });
 	console.log(req);
 };
