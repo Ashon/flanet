ade6162fc94da11798df718ade1a450e45ef6dd2
ashon <ashon8813@gmail.com>
2013-12-23 00:16:52 +0900

    asdf

--- app.js
+++ app.js
@@ -23,29 +23,29 @@ passport.deserializeUser(function (obj, done) {
 	done(null, obj);
 });
 
-
 // Use the FacebookStrategy within Passport.
 //   Strategies in Passport require a `verify` function, which accept
 //   credentials (in this case, an accessToken, refreshToken, and Facebook
 //   profile), and invoke a callback with a user object.
 passport.use(new fbStrategy({
-	clientID: FACEBOOK_APP_ID,
-	clientSecret: FACEBOOK_APP_SECRET,
-	callbackURL: "http://flanet.herokuapp.com/auth/facebook/callback"
-},
-function (accessToken, refreshToken, profile, done) {
-		// asynchronous verification, for effect...
-		process.nextTick(function () {
-			// set accesstoken
-			profile.token = accessToken;
-			// To keep the example simple, the user's Facebook profile is returned to
-			// represent the logged-in user.  In a typical application, you would want
-			// to associate the Facebook account with a user record in your database,
-			// and return that user instead.
-			return done(null, profile);
-		});
-	}
-	));
+		clientID: FACEBOOK_APP_ID,
+		clientSecret: FACEBOOK_APP_SECRET,
+		callbackURL: "http://flanet.herokuapp.com/auth/facebook/callback"
+	},
+	function (accessToken, refreshToken, profile, done) {
+			// asynchronous verification, for effect...
+			process.nextTick(function () {
+				// set accesstoken
+				profile.token = accessToken;
+				// To keep the example simple, the user's Facebook profile is returned to
+				// represent the logged-in user.  In a typical application, you would want
+				// to associate the Facebook account with a user record in your database,
+				// and return that user instead.
+				return done(null, profile);
+			});
+		}
+	)
+);
 
 var port = process.env.PORT || 3000;
 
